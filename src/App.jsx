31c0af611f96b56892bcd9ae6b8e94f535a86bf4
onyyyyx <onyyyyx@proton.me>
2024-11-19 21:07:55 +0100

    Reset all

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,5 @@
-import { useState, useEffect } from "react";
+function App () {
 
-function App() {
-  function hash(string) {
-    const utf8 = new TextEncoder().encode(string);
-    return crypto.subtle.digest("SHA-256", utf8).then((hashBuffer) => {
-      const hashArray = Array.from(new Uint8Array(hashBuffer));
-      const hashHex = hashArray
-        .map((bytes) => bytes.toString(16).padStart(2, "0"))
-        .join("");
-      return hashHex;
-    });
-  }
-  const queryParams = new URLSearchParams(window.location.search);
-  const token = queryParams.get("token");
-  const isTokenValid = hash(token);
-
-  return <p>{isTokenValid}</p>;
 }
 
 export default App;
